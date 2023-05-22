4d482b353a0e757083badee5a01dbf693923515c
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-05-22 22:16:42 +0530

    #include<stdio.h> int main() { 	int n,i,temp; 	scanf("%d",&n);     for(i=1;i<=n;i++)     {     	if(i*i==n)     	{     		temp=i; 		} 	} 	if(temp*temp==n) 	{ 		printf("True"); 	} 	else 	{ 		printf("False"); 	} }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,temp;
-    scanf("%d",&n);//16 =4*4
+	int n,i,temp;
+	scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-       if(i*i==n)
-       {
-           temp=i;
-       }
-       
-    }
-    if(temp*temp==n)
-    {
-        printf("True");
-    }
-    else
-    {
-        printf("False");
-    }
-    
+    	if(i*i==n)
+    	{
+    		temp=i;
+		}
+	}
+	if(temp*temp==n)
+	{
+		printf("True");
+	}
+	else 
+	{
+		printf("False");
+	}
 }
