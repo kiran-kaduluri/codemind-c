de194197bab100116844459fdd7c6faae3b0a2a8
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-05-22 22:05:26 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);    if(n<9999999999&&n>100000000)    {         printf("Valid");     }     else     {         printf("Invalid");     } }

--- PHONE_NUMBER_VALIDATION:.c
+++ PHONE_NUMBER_VALIDATION:.c
@@ -1,15 +1,13 @@
 #include<stdio.h>
 int main()
 {
-  int n;
-  scanf("%d",&n);
-  if(n<9999999999&&n>100000000)
-  {
-  printf("Valid");
-  }
-  else 
-  {
-  printf("Invalid");
-  }
-  
+    int n;
+    scanf("%d",&n);
+   if(n<9999999999&&n>100000000)    {
+        printf("Valid");
+    }
+    else 
+    {
+        printf("Invalid");
+    }
 }
