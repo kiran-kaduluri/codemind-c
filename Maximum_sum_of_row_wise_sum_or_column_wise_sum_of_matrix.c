7e4ff51e66fc3fb2c41d47aad1ef58d7cb1e5944
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-05-24 22:58:10 +0530

    #include<stdio.h> int main() {     int r,c,max1=0,max2=0;     scanf("%d%d",&r,&c);     int mat[r][c];     for(int i=0;i<r;i++)     {         for(int j=0;j<c;j++)         {             scanf("%d",&mat[i][j]);         }     }     for(int i=0;i<r;i++)     {         int s=0;         for(int j=0;j<c;j++)         {             s=s+mat[i][j];         }         if(s>max1)         {             max1=s;         }     }     for(int i=0;i<c;i++)     {         int s=0;         for(int j=0;j<r;j++)         {             s=s+mat[j][i];         }         if(s>max2)         {             max2=s;         }     }     if(max1>max2)     {         printf("%d",max1);     }     else     {         printf("%d",max2);     } }

---
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,max1=0,max2=0;
+    scanf("%d%d",&r,&c);
+    int mat[r][c];
+    for(int i=0;i<r;i++)
+    {
+        for(int j=0;j<c;j++)
+        {
+            scanf("%d",&mat[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++)
+    {
+        int s=0;
+        for(int j=0;j<c;j++)
+        {
+            s=s+mat[i][j];
+        }
+        if(s>max1)
+        {
+            max1=s;
+        }
+    }
+    for(int i=0;i<c;i++)
+    {
+        int s=0;
+        for(int j=0;j<r;j++)
+        {
+            s=s+mat[j][i];
+        }
+        if(s>max2)
+        {
+            max2=s;
+        }
+    }
+    if(max1>max2)
+    {
+        printf("%d",max1);
+    }
+    else
+    {
+        printf("%d",max2);
+    }
+}
+
