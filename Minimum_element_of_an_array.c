a2ef00c37c4f2555d0104770afb259d84eac1a01
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-05-24 20:27:48 +0530

    #include<stdio.h> int main() { 	int size,i,max; 	scanf("%d",&size); 	int a[size]; 	for(i=0;i<size;i++) 	{ 		scanf("%d",&a[i]); 	} 	max=a[0]; 	for(i=1;i<size;i++) 	{ 		if(a[i]<max) 		{ 			max=a[i]; 		} 	} 	printf("%d",max); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int size,i,max;
+	scanf("%d",&size);
+	int a[size];
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=1;i<size;i++)
+	{
+		if(a[i]<max)
+		{
+			max=a[i];
+		}
+	}
+	printf("%d",max);
+}
