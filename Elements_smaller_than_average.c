a7524f552df66e645a1e501e430a436a6817c721
Kiran_kaduluri <119723055+kiran-kaduluri@users.noreply.github.com>
2023-05-24 20:11:19 +0530

    #include<stdio.h> int main() {     int s;     scanf("%d",&s);     int a[s],i,sum=0,avg,c=0;     for(i=0;i<s;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<s;i++)     {         sum=sum+a[i];     }     avg=sum/s;     for(i=0;i<s;i++)     {         if(avg>=a[i])         {             c++;         }     }     printf("%d",c); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int s;
+    scanf("%d",&s);
+    int a[s],i,sum=0,avg,c=0;
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<s;i++)
+    {
+        sum=sum+a[i];
+    }
+    avg=sum/s;
+    for(i=0;i<s;i++)
+    {
+        if(avg>=a[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
